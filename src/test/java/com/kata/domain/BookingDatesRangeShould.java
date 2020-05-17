3c5f8b88cc49473b246abab23e672cad9505903b
Isidro Lopez <islomar@gmail.com>
2020-05-17 14:12:00 +0200

    Improve displayed test cases for date ranges

--- src/test/java/com/kata/domain/BookingDatesRangeShould.java
+++ src/test/java/com/kata/domain/BookingDatesRangeShould.java
@@ -14,7 +14,7 @@ class BookingDatesRangeShould {
 	private static final LocalDate TOMORROW = LocalDate.of(2019, 1, 2);
 	private static final LocalDate TODAY = LocalDate.of(2019, 1, 1);
 
-	@ParameterizedTest
+	@ParameterizedTest(name = "\"{0}\" should be before {1}")
 	@MethodSource("provideInvalidDateRanges")
 	public void throw_exception_when_checkout_date_is_prior_to_the_checkin_date(LocalDate checkIn, LocalDate checkOut) {
 		assertThrows(InvalidDateRangeException.class, () -> {
