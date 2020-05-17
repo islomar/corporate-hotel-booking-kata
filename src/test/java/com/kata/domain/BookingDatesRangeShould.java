31570d88c2fa28d3201a8fb81f102e9b2e03dbaf
Isidro Lopez <islomar@gmail.com>
2020-05-17 14:24:30 +0200

    Refactor to oneline lambda call

--- src/test/java/com/kata/domain/BookingDatesRangeShould.java
+++ src/test/java/com/kata/domain/BookingDatesRangeShould.java
@@ -19,9 +19,7 @@ class BookingDatesRangeShould {
 	@ParameterizedTest(name = "\"{0}\" should be before {1}")
 	@MethodSource("provideInvalidDateRanges")
 	public void throw_exception_when_checkout_date_is_prior_to_the_checkin_date(LocalDate checkIn, LocalDate checkOut) {
-		assertThrows(InvalidDateRangeException.class, () -> {
-			new BookingDatesRange(checkIn, checkOut);
-		});
+		assertThrows(InvalidDateRangeException.class, () -> new BookingDatesRange(checkIn, checkOut));
 	}
 
 	private static Stream<Arguments> provideInvalidDateRanges() {
