b3d58f88f2f4d1f002c6ae45db0b6760d285a36a
Isidro Lopez <islomar@gmail.com>
2020-06-21 13:23:38 +0200

    Refactor BookingService test: extract common arrengements to setUp()

--- src/test/java/com/kata/application/BookingServiceShould.java
+++ src/test/java/com/kata/application/BookingServiceShould.java
@@ -5,6 +5,7 @@ import com.kata.domain.*;
 import com.kata.infrastructure.InMemoryBookingRepository;
 import com.kata.infrastructure.InMemoryCompanyRepository;
 import com.kata.infrastructure.InMemoryHotelRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -16,69 +17,64 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BookingServiceShould {
 
-	@Test
-	public void book_a_room() throws InvalidDateRangeException, HotelNotFoundException, NonExistingRoomTypeException {
-		CompanyService companyService = new CompanyService(new InMemoryCompanyRepository());
-		companyService.addEmployee("anyCompanyId", "anyEmployeeId");
-		HotelRepository hotelRepository = new InMemoryHotelRepository();
-		HotelService hotelService = new HotelService(hotelRepository);
-		hotelService.addHotel("anyHotelId", "anyHotelName");
-		hotelService.setRoom("anyHotelId", "anyRoomNumber", "anyRoomType");
-		BookingRepository bookingRepository = new InMemoryBookingRepository();
-		BookingService bookingService = new BookingService(bookingRepository, hotelRepository);
-		LocalDate checkIn = LocalDate.of(2019, 1, 1);
-		LocalDate checkOut = LocalDate.of(2019, 1, 2);
+    private static final LocalDate TODAY = LocalDate.of(2019, 1, 1);
+    private static final LocalDate TOMORROW = LocalDate.of(2019, 1, 2);
+    private HotelService hotelService;
+    private BookingService bookingService;
+    private BookingRepository bookingRepository;
+    private HotelRepository hotelRepository;
 
-		Booking booking = bookingService.book("anyEmployeeId", "anyHotelId", "anyRoomType", checkIn, checkOut);
+    @BeforeEach
+    public void setUp() {
+        CompanyService companyService = new CompanyService(new InMemoryCompanyRepository());
+        companyService.addEmployee("anyCompanyId", "anyEmployeeId");
+        hotelRepository = new InMemoryHotelRepository();
+        hotelService = new HotelService(hotelRepository);
+        bookingRepository = new InMemoryBookingRepository();
+        bookingService = new BookingService(bookingRepository, hotelRepository);
+    }
 
-		Booking foundBooking = bookingRepository.findById(booking.getId());
-		assertThat(foundBooking, is(booking));
-	}
 
-	@Test
-	public void not_proceed_with_the_booking_if_checkout_date_is_at_least_one_day_after_checkin_date() throws HotelNotFoundException {
-		InMemoryBookingRepository bookingRepository = new InMemoryBookingRepository();
-		HotelRepository hotelRepository = new InMemoryHotelRepository();
-		HotelService hotelService = new HotelService(hotelRepository);
-		hotelService.addHotel("anyHotelId", "anyHotelName");
-		hotelService.setRoom("anyHotelId", "anyRoomNumber", "anyRoomType");
-		BookingService bookingService = new BookingService(bookingRepository, hotelRepository);
-		LocalDate checkIn = LocalDate.of(2019, 1, 2);
-		LocalDate checkOut = LocalDate.of(2019, 1, 1);
+    @Test
+    public void book_a_room() throws InvalidDateRangeException, HotelNotFoundException, NonExistingRoomTypeException {
+        hotelService.addHotel("anyHotelId", "anyHotelName");
+        hotelService.setRoom("anyHotelId", "anyRoomNumber", "anyRoomType");
 
-		assertThrows(InvalidDateRangeException.class, () -> bookingService.book("anyEmployeeId", "anyHotelId", "anyRoomType", checkIn, checkOut));
-		assertThat(bookingRepository.findAll(), is(empty()));
-	}
+        Booking booking = bookingService.book("anyEmployeeId", "anyHotelId", "anyRoomType", TODAY, TOMORROW);
 
-	@Test
-	public void not_proceed_with_the_booking_if_hotel_does_not_exist() {
-		CompanyService companyService = new CompanyService(new InMemoryCompanyRepository());
-		companyService.addEmployee("anyCompanyId", "anyEmployeeId");
-		InMemoryBookingRepository bookingRepository = new InMemoryBookingRepository();
-        HotelRepository hotelRepository = new InMemoryHotelRepository();
-		BookingService bookingService = new BookingService(bookingRepository, hotelRepository);
-		LocalDate checkIn = LocalDate.of(2019, 1, 1);
-		LocalDate checkOut = LocalDate.of(2019, 1, 2);
+        Booking foundBooking = bookingRepository.findById(booking.getId());
+        assertThat(foundBooking, is(booking));
+    }
 
-		assertThrows(HotelNotFoundException.class, () -> bookingService.book("anyEmployeeId", "aNonExistingHotelId", "anyRoomType", checkIn, checkOut));
+    @Test
+    public void not_proceed_with_the_booking_if_checkout_date_is_at_least_one_day_after_checkin_date() throws HotelNotFoundException {
+        InMemoryBookingRepository bookingRepository = new InMemoryBookingRepository();
+        hotelService.addHotel("anyHotelId", "anyHotelName");
+        hotelService.setRoom("anyHotelId", "anyRoomNumber", "anyRoomType");
+        BookingService bookingService = new BookingService(bookingRepository, hotelRepository);
+
+        assertThrows(InvalidDateRangeException.class, () -> bookingService.book("anyEmployeeId", "anyHotelId", "anyRoomType", TOMORROW, TODAY));
+        assertThat(bookingRepository.findAll(), is(empty()));
+    }
+
+    @Test
+    public void not_proceed_with_the_booking_if_hotel_does_not_exist() {
+        InMemoryBookingRepository bookingRepository = new InMemoryBookingRepository();
+        BookingService bookingService = new BookingService(bookingRepository, hotelRepository);
+
+        assertThrows(HotelNotFoundException.class, () -> bookingService.book("anyEmployeeId", "aNonExistingHotelId", "anyRoomType", TODAY, TOMORROW));
         assertThat("The booking should not have been done", bookingRepository.findAll(), is(empty()));
-	}
+    }
 
-	@Test
-	public void not_proceed_with_the_booking_if_room_type_does_not_exist() throws HotelNotFoundException {
-		CompanyService companyService = new CompanyService(new InMemoryCompanyRepository());
-		companyService.addEmployee("anyCompanyId", "anyEmployeeId");
-		HotelRepository hotelRepository = new InMemoryHotelRepository();
-		HotelService hotelService = new HotelService(hotelRepository);
-		hotelService.addHotel("anyHotelId", "anyHotelName");
-		hotelService.setRoom("anyHotelId", "anyRoomNumber", "anyRoomType");
-		InMemoryBookingRepository bookingRepository = new InMemoryBookingRepository();
-		BookingService bookingService = new BookingService(bookingRepository, hotelRepository);
-		LocalDate checkIn = LocalDate.of(2019, 1, 1);
-		LocalDate checkOut = LocalDate.of(2019, 1, 2);
+    @Test
+    public void not_proceed_with_the_booking_if_room_type_does_not_exist() throws HotelNotFoundException {
+        hotelService.addHotel("anyHotelId", "anyHotelName");
+        hotelService.setRoom("anyHotelId", "anyRoomNumber", "anyRoomType");
+        InMemoryBookingRepository bookingRepository = new InMemoryBookingRepository();
+        BookingService bookingService = new BookingService(bookingRepository, hotelRepository);
 
-		assertThrows(NonExistingRoomTypeException.class, () -> bookingService.book("anyEmployeeId", "anyHotelId", "nonExistingRoomType", checkIn, checkOut));
-		assertThat("The booking should not have been done", bookingRepository.findAll(), is(empty()));
-	}
+        assertThrows(NonExistingRoomTypeException.class, () -> bookingService.book("anyEmployeeId", "anyHotelId", "nonExistingRoomType", TODAY, TOMORROW));
+        assertThat("The booking should not have been done", bookingRepository.findAll(), is(empty()));
+    }
 
 }
